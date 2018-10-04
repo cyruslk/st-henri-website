b4f532d06eb9a65ee46f5aa84a3f2fa28988014d
cyruslk <cyrus7@hotmail.fr>
2018-10-04 14:43:05 -0400

    changing the pos of the methods

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "./App.css";
 
 class App extends React.Component {
  state = {
-  allData: [
+  indexOfTheSelectedComponent: "1",
+  allDataImages: [
     { name: "collection1_img",
       type: "img",
       visible: true,
@@ -58,15 +59,68 @@ class App extends React.Component {
         ]
       }]
     }
+  ],
+  allDataTexts: [
+    { name: "collection1_text",
+      type: "img",
+      visible: true,
+      categories: [{
+        name: "information",
+        visible: true,
+        content: [
+          "img1",
+          "img2"
+        ]
+      },{
+        name: "pressrelease",
+        visible: true,
+        content: [
+          "img1",
+          "img2"
+        ]
+      },{
+        name: "pressrelease2",
+        visible: false,
+        content: [
+          "img1",
+          "img2"
+        ]
+      }]
+    },
+    { name: "collection1_text",
+      type: "img",
+      visible: false,
+      categories: [{
+        name: "information",
+        visible: true,
+        content: [
+          "img1",
+          "img2"
+        ]
+      },{
+        name: "pressrelease",
+        visible: false,
+        content: [
+          "img1",
+          "img2"
+        ]
+      },{
+        name: "pressrelease2",
+        visible: false,
+        content: [
+          "img1",
+          "img2"
+        ]
+      }]
+    }
   ]
   };
 
  displaySelectedComponent = (e, component) => {
 
-  const indexElement = this.state.allData.indexOf(component);
-  let newState = [...this.state.allData];
+  const indexElement = this.state.allDataImages.indexOf(component);
+  let newState = [...this.state.allDataImages];
   newState[indexElement].visible = !newState[indexElement].visible;
-
   const modifiedListArray = [];
   newState.filter((ele, index) => {
     if(ele.visible === true){
@@ -74,50 +128,74 @@ class App extends React.Component {
     }
   })
   this.setState({
-    allData: newState
+    allDataImages: newState,
+    indexOfTheSelectedComponent: indexElement
   });
  };
 
 
- displaySubSelectedComponent = (e, index, component) => {
-   const indexElement = this.state.allData.indexOf(component);
-   let newState = [...this.state.allData];
+
+ displaySubSelectedComponentImgs = (e, index, component) => {
+
+   const indexElement = this.state.allDataImages.indexOf(component);
+   let newState = [...this.state.allDataImages];
    newState[indexElement].categories[index].visible = !newState[indexElement].categories[index].visible;
-   // either this or a filter loop to add and remove these subcomponents to the [];
-   // need to be confirmed with M.
    this.setState({
-     allData: newState
+     allDataImages: newState
    });
  };
 
 
-
  render() {
+
+  const firstLayerButtonsImg = this.state.allDataImages.map((data, index) => {
+    return (
+        <div key={index}>
+         <button onClick={e => this.displaySelectedComponent(e, data)}>
+          {data.name}
+         </button>
+         </div>
+     )
+  })
+
+  const firstLayerButtonsTxts = this.state.allDataTexts.map((data, index) => {
+    return (
+      <div key={index}>
+       <button onClick={e => this.displaySelectedComponent(e, data)}>
+        {data.name}
+       </button>
+      </div>
+     )
+  })
+
+  const SecondLayerButtonsImg = this.state.allDataImages
+  [this.state.indexOfTheSelectedComponent].categories.map((data, index) => {
+    return (
+        <div key={index}>
+         <button onClick={e => this.displaySelectedComponent(e, data)}>
+          {data.name}
+         </button>
+         </div>
+     )
+  })
+
+  alert(this.state.indexOfTheSelectedComponent)
+
   return (
    <div className="App">
-    {this.state.allData.map((data, index) => (
+   <header>
+     <section className="buttons_left">
+     <div>{firstLayerButtonsImg}</div>
+     <div>{SecondLayerButtonsImg}</div>
+     </section>
+     <section className="buttons_right">
+     {firstLayerButtonsTxts}
+     </section>
+   </header>
 
-     <div key={index} id="all_buttons_of_cms">
-      <button onClick={e => this.displaySelectedComponent(e, data)}>
-       {data.name}
-      </button>
-     </div>
-    ))}
-
-    {this.state.allData.map(component => {
+    {this.state.allDataImages.map(component => {
      if (component.visible
       && component.type === "img") {
-
-      const renderButtonSubtypes = component.categories.map((ele, index) => {
-        return (
-          <div key={index} id="sub_buttons_of_collections">
-           <button onClick={e => this.displaySubSelectedComponent(e, index, component)}>
-            {ele.name}
-           </button>
-          </div>
-        )
-
-      })
 
       const selectedSubType = component.categories.map((ele, index) => {
         if(ele.visible && ele.name === "compaign"){
@@ -144,10 +222,7 @@ class App extends React.Component {
       })
 
       return (
-        <div key={Math.random().toString(36).substring(7)}>
-            <section className="subtypes">
-              {renderButtonSubtypes}
-            </section>
+        <div key={Math.random().toString(36).substring(7)} id="test">
             <section>
             <div id="category">
             {component.name}

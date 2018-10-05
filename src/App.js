6ab6834f570861ba647f904bd6eb10ad95219fe8
cyruslk <cyrus7@hotmail.fr>
2018-10-05 15:59:01 -0400

    committing bf changing

--- src/App.js
+++ src/App.js
@@ -4,108 +4,139 @@ import ReactDOM from "react-dom";
 import "./App.css";
 
 class App extends React.Component {
- state = {
-  indexOfTheSelectedComponent: "0",
-  allDataImages: [
-    {
-      collection: 1,
-      type: "edito",
-      content:
-        [
-          "img1",
-          "img2"
-        ]
-    },
-    {
-      collection: 1,
-      type: "lookbook",
-      content:
-        [
-          "img1",
-          "img2"
-        ]
-    },
-    {
-      collection: 1,
-      type: "campaign",
-      content:
-        [
-          "img1",
-          "img2"
-        ]
-    },
-    {
-      collection: 1,
-      type: "press_release",
-      content:
-        [
-          "dvfddvdvdf",
-          "dvfddvdvdf"
-        ]
-    },
-    {
-      collection: 1,
-      type: "press",
-      content:
-        [
-          "dvfddvdvdf",
-          "dvfddvdvdf"
-        ]
-    },
-    {
-      collection: 2,
-      type: "edito",
-      content:
-        [
-          "img1",
-          "img2"
-        ]
-    },
-    {
-      collection: 2,
-      type: "lookbook",
-      content:
-        [
-          "img1",
-          "img2"
-        ]
-    },
-    {
-      collection: 2,
-      type: "campaign",
-      content:
-        [
-          "img1",
-          "img2"
-        ]
-    },
-    {
-      collection: 2,
-      type: "press_release",
-      content:
-        [
-          "dvfddvdvdf",
-          "dvfddvdvdf"
-        ]
-    },
-    {
-      collection: 2,
-      type: "press",
-      content:
-        [
-          "dvfddvdvdf",
-          "dvfddvdvdf"
-        ]
-    }
-  ]
-  };
 
+
+  constructor(props) {
+   super(props);
+   this.state = {
+    indexOfTheSelectedComponent: "0",
+    renderQueue: "",
+    allData: [
+      {
+        collection: 1,
+        status: true,
+        type: "edito",
+        content:
+          [
+            "img1",
+            "img2"
+          ]
+      },
+      {
+        collection: 1,
+        status: false,
+        type: "lookbook",
+        content:
+          [
+            "img1",
+            "img2"
+          ]
+      },
+      {
+        collection: 1,
+        status: false,
+        type: "campaign",
+        content:
+          [
+            "img1",
+            "img2"
+          ]
+      },
+      {
+        collection: 1,
+        status: false,
+        type: "press_release",
+        content:
+          [
+            "dvfddvdvdf",
+            "dvfddvdvdf"
+          ]
+      },
+      {
+        collection: 1,
+        status: false,
+        type: "press",
+        content:
+          [
+            "dvfddvdvdf",
+            "dvfddvdvdf"
+          ]
+      },
+      {
+        collection: 2,
+        status: false,
+        type: "edito",
+        content:
+          [
+            "img1",
+            "img2"
+          ]
+      },
+      {
+        collection: 2,
+        status: false,
+        type: "lookbook",
+        content:
+          [
+            "img1",
+            "img2"
+          ]
+      },
+      {
+        collection: 2,
+        status: false,
+        type: "campaign",
+        content:
+          [
+            "img1",
+            "img2"
+          ]
+      },
+      {
+        collection: 2,
+        status: false,
+        type: "press_release",
+        content:
+          [
+            "dvfddvdvdf",
+            "dvfddvdvdf"
+          ]
+      },
+      {
+        collection: 2,
+        status: true,
+        type: "press",
+        content:
+          [
+            "dvfddvdvdf",
+            "dvfddvdvdf"
+          ]
+      }
+    ]
+    };
+ }
+
+ componentDidMount(){
+   const renderedQueueArray = [];
+   const returnRenderQueue = this.state.allData.map((ele, index) => {
+     console.log(ele);
+     if(ele.status === true){
+       renderedQueueArray.push(ele);
+     }
+   })
+   console.log("----", renderedQueueArray);
+
+   this.setState({
+     renderQueue: renderedQueueArray
+   })
+ }
 
  render() {
+
   return (
    <div className="App">
    <header>
-
+    --
    </header>
    <main>
    </main>
